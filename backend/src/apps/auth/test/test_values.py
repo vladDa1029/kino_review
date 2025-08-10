964fd5ef88bd26ab0d27ec0d67016f0811a594c0
vladislav1029 <vlad1090904@gmail.com>
2025-08-10 21:17:54 +0300

    удалены тесты

--- backend/src/apps/auth/test/test_values.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from app.domain.values import Payload
-
-
-def test_values_to_dict():
-    sub_uuid = str(uuid.uuid4())
-    pay = Payload(sub=sub_uuid, add_exp= 500)
-    assert  pay.to_dict() and not pay.is_expired()
